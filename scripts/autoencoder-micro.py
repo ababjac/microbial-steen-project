e0be5c856c96ae1e0e90244741de8310deaa061b
Ashley Babjac <ababjac@vols.utk.edu>
2021-11-01 16:23:46 -0400

    adding latest changes to autoencoder

--- scripts/autoencoder-micro.py
+++ scripts/autoencoder-micro.py
@@ -4,30 +4,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import svm, metrics
 from sklearn.model_selection import train_test_split
-import keras
+from tensorflow import keras as ks
 
-def plot_confusion_matrix(y_pred, y_actual, title, filename):
-    cf_matrix = metrics.confusion_matrix(y_actual, y_pred)
-    if len(cf_matrix) != 2: #if it predicts perfectly then confusion matrix returns incorrect form
-        val = cf_matrix[0][0]
-        tmp = [val, 0]
-        cf_matrix = np.array([tmp, [0, 0]])
+def declare_autoencoder(num_features, latent_space = 50):
+    print("Instantiate Autoencoder...")
+    autoencoder = ks.Sequential([
+        ks.layers.Dense(num_features,input_shape=(num_features,)),
+        ks.layers.Dense(latent_space,activation='relu'),
+        ks.layers.Dense(num_features,activation='sigmoid')
+    ])
 
-    #print(cf_matrix)
+    autoencoder.summary()
 
-    ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues')
+    opt = ks.optimizers.Adam(learning_rate=0.1)
+    autoencoder.compile(optimizer=opt,loss='binary_crossentropy')
 
-    ax.set_title(title+'\n\n');
-    ax.set_xlabel('\nPredicted Values')
-    ax.set_ylabel('Actual Values\n');
-
-    ## Ticket labels - List must be in alphabetical order
-    ax.xaxis.set_ticklabels(['False','True'])
-    ax.yaxis.set_ticklabels(['False','True'])
-
-    ## Display the visualization of the Confusion Matrix.
-    plt.savefig('images/confusion-matrix/std-SVM/'+filename)
-    plt.close()
+    return autoencoder
 
 print('Reading data...')
 data = pd.read_csv('files/data/condensedKO_features.csv', index_col=0)
@@ -44,30 +36,10 @@ print('Splitting data...')
 features = data.loc[:, ~data.columns.isin(['site'])]
 features = pd.get_dummies(features)
 
-labels = labels.loc[:, ~labels.columns.isin(['site'])]
-
 print('Cleaning features...')
 remove = [col for col in features.columns if features[col].isna().sum() != 0 or col.__contains__('Ocean.region')]
 features = features.loc[:, ~features.columns.isin(remove)] #remove columns with too many missing values
 
-
-# print()
-# for label in labels.columns:
-#     X_train, X_test, y_train, y_test = train_test_split(features, labels[label], test_size=0.3,random_state=5) # 70% training and 30% test
-#
-#     print('Building model for label:', label)
-#     clf = svm.SVC(kernel='linear')
-#     clf.fit(X_train, y_train)
-#
-#     print('Predicting on test data for label:', label)
-#     y_pred = clf.predict(X_test)
-#
-#     print('Calculating metrics for:', label)
-#     print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-#     print("Precision:",metrics.precision_score(y_test, y_pred))
-#     print("Recall:",metrics.recall_score(y_test, y_pred))
-#
-#     print('Plotting:', label)
-#     plot_confusion_matrix(y_pred=y_pred, y_actual=y_test, title=label, filename=label+'_CM.png')
-#
-#     print()
+print('Training model...')
+AE = declare_autoencoder(num_features=len(features.columns), latent_space = 9)
+AE.fit(features, features, epochs=5, batch_size=1)

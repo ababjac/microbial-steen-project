b4bfdaac32a95f721c469a59b53d08d67d5092be
Ashley Babjac <ababjac@vols.utk.edu>
2022-04-02 09:47:10 -0400

    latest changes to code

--- scripts/AE-TARA.py
+++ scripts/AE-TARA.py
@@ -106,8 +106,8 @@ print()
 params_SVM = {
     'C': [0.1, 1, 10, 100, 1000],
     'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
-    'kernel': ['rbf', 'linear']#,
-    #'probability': [True]
+    'kernel': ['rbf', 'linear'],
+    'probability': [True]
 }
 
 clf = GridSearchCV(
